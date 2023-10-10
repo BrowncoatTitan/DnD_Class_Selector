2c1deb52c85f08afbc12abfa57c97f430fbdb1b9
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-10-10 09:43:49 -0500

    git init

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+﻿using D_D_Class_Selector.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace D_D_Class_Selector.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace D_D_Class_Selector.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
