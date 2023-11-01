ffb9cbd56ee0122e1bdb482c26d3c07b1ab78257
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-11-01 10:24:29 -0500

    refactoring questions

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -11,53 +11,53 @@ namespace D_D_Class_Selector.Controllers
             {
                 new QuestionModel
                 { 
-                    Text = "I want to use magic to solve all of my problems."
-                
+                    Text = "I want to use magic to solve all of my problems.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "I want to rely on my own cunning and prowess."
-                
+                    Text = "I want to rely on my own cunning and prowess.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "I prefer to support my allies rather than rely on myself."
-                
+                    Text = "I prefer to support my allies rather than rely on myself.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "I want to be in the thick of the fight."
-                
+                    Text = "I want to be in the thick of the fight.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "I want to focus on out of combat utility."
-                
+                    Text = "I want to focus on out of combat utility.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "I believe if you are strong enough, problems go away."
-                
+                    Text = "I believe if you are strong enough, problems go away.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "My allies are precious; I will protect them."
-                
+                    Text = "My allies are precious; I will protect them.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "Mastering one's self is the key to mastering the world"
-                
+                    Text = "Mastering one's self is the key to mastering the world",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "A little luck is all I need."
-                
+                    Text = "A little luck is all I need.",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 },
                 new QuestionModel
                 {
-                    Text = "The world was meant to be explored"
-                
+                    Text = "The world was meant to be explored",
+                    Options = new List<string> { "1", "2", "3", "4", "5" }
                 }
             };
             return questions;
