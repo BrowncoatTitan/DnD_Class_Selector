c759314d81122360de25235570ba04cab214f37b
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-11-01 10:22:03 -0500

    refactoring questions

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -9,16 +9,56 @@ namespace D_D_Class_Selector.Controllers
         {
             var questions = new List<QuestionModel>
             {
-                new QuestionModel{ Text = "I want to use magic to solve all of my problems."},
-                new QuestionModel{ Text = "I want to rely on my own cunning and prowess."},
-                new QuestionModel{ Text = "I prefer to support my allies rather than rely on myself."},
-                new QuestionModel{ Text = "My goals are not important, I do the will of those I serve."},
-                new QuestionModel{ Text = "I rely not on my power, but on that of the gods."},
-                new QuestionModel{ Text = "I believe if you are strong enough, problems go away."},
-                new QuestionModel{ Text = "My friends are precious; I will protect them."},
-                new QuestionModel{ Text = "Mastering one's self is the key to mastering the world"},
-                new QuestionModel{ Text = "A little luck is all I need."},
-                new QuestionModel{ Text = "The world was meant to be explored"}
+                new QuestionModel
+                { 
+                    Text = "I want to use magic to solve all of my problems."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "I want to rely on my own cunning and prowess."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "I prefer to support my allies rather than rely on myself."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "I want to be in the thick of the fight."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "I want to focus on out of combat utility."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "I believe if you are strong enough, problems go away."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "My allies are precious; I will protect them."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "Mastering one's self is the key to mastering the world"
+                
+                },
+                new QuestionModel
+                {
+                    Text = "A little luck is all I need."
+                
+                },
+                new QuestionModel
+                {
+                    Text = "The world was meant to be explored"
+                
+                }
             };
             return questions;
 
--- Models/QuestionModel.cs
+++ Models/QuestionModel.cs
@@ -1,8 +1,10 @@
-﻿namespace D_D_Class_Selector.Models
+﻿using D_D_Class_Selector.Models;
+
+namespace D_D_Class_Selector.Models
 {
     public class QuestionModel
     {
         public string Text { get; set; }
-        public int Response { get; set; }
+        public List<string> Options { get; set; }
     }
 }
