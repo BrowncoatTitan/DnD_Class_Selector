deb4723b389c5601ff13726e8bdf949a7c86b694
Taylor McGregor <mcgregor.taylor44@gmail.com>
2023-11-13 14:06:50 -0600

    began retooling to be a party manager

--- Controllers/QuestionController.cs
+++ Controllers/QuestionController.cs
@@ -1,72 +1,72 @@
 ﻿using D_D_Class_Selector.Models;
-using Microsoft.AspNetCore.Mvc;
+//using Microsoft.AspNetCore.Mvc;
 
-namespace D_D_Class_Selector.Controllers
-{
-    public class QuestionController : Controller
-    {
-        private List<QuestionModel> GetQuestions()
-        {
-            var questions = new List<QuestionModel>
-            {
-                new QuestionModel
-                { 
-                    Text = "I want to use magic to solve all of my problems.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "I want to rely on my own cunning and prowess.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "I prefer to support my allies rather than rely on myself.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "I want to be in the thick of the fight.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "I want to focus on out of combat utility.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "I believe if you are strong enough, problems go away.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "My allies are precious; I will protect them.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "Mastering one's self is the key to mastering the world",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "A little luck is all I need.",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                },
-                new QuestionModel
-                {
-                    Text = "The world was meant to be explored",
-                    Options = new List<string> { "1", "2", "3", "4", "5" }
-                }
-            };
-            return questions;
+//namespace D_D_Class_Selector.Controllers
+//{
+//    public class QuestionController : Controller
+//    {
+//        private List<QuestionModel> GetQuestions()
+//        {
+//            var questions = new List<QuestionModel>
+//            {
+//                new QuestionModel
+//                { 
+//                    Text = "I want to use magic to solve all of my problems.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "I want to rely on my own cunning and prowess.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "I prefer to support my allies rather than rely on myself.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "I want to be in the thick of the fight.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "I want to focus on out of combat utility.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "I believe if you are strong enough, problems go away.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "My allies are precious; I will protect them.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "Mastering one's self is the key to mastering the world",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "A little luck is all I need.",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                },
+//                new QuestionModel
+//                {
+//                    Text = "The world was meant to be explored",
+//                    Options = new List<string> { "1", "2", "3", "4", "5" }
+//                }
+//            };
+//            return questions;
 
-        }
-        public IActionResult Index()
-        {
-            var questions = GetQuestions();
-            return View(questions);
-        }
-    }
+//        }
+//        public IActionResult Index()
+//        {
+//            var questions = GetQuestions();
+//            return View(questions);
+//        }
+//    }
 }
--- Models/QuestionModel.cs
+++ Models/QuestionModel.cs
@@ -1,10 +1,10 @@
-﻿using D_D_Class_Selector.Models;
+﻿//using D_D_Class_Selector.Models;
 
-namespace D_D_Class_Selector.Models
-{
-    public class QuestionModel
-    {
-        public string Text { get; set; }
-        public List<string> Options { get; set; }
-    }
-}
+//namespace D_D_Class_Selector.Models
+//{
+//    public class QuestionModel
+//    {
+//        public string Text { get; set; }
+//        public List<string> Options { get; set; }
+//    }
+//}
